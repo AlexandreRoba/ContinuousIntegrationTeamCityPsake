e82fd171c7d496c0385b31d4fe86043e8671d666
Alexandre Roba <alexandre.roba@arobanet.be>
2016-05-23 11:04:33 +0200

    Initial Commit

---
+++ psake/Build/build.ps1
@@ -0,0 +1,12 @@
+﻿cls
+
+# '[p]sake is the same as 'psake' but $Error is not polluted if the module is not loaded
+Remove-Module [p]sake
+
+# find psake's path
+$psakeModule = (Get-ChildItem ('..\Packages\psake*\tools\psake.psm1')).FullName | Sort-Object $_ | select -Last 1
+
+# Import the module
+Import-Module $psakeModule
+
+Invoke-psake -buildFile .\default.ps1 -taskList Test
---
+++ psake/Build/default.ps1
@@ -0,0 +1,19 @@
+﻿properties{
+	$CleanMessage = "Executed Clean!"
+	$CompileMessage = "Executed Compile !"
+	$TestMessage = "Executed unit tests!"
+}
+
+task default -depends Test
+
+task Clean -description "Clean the build output"{
+	Write-Host $CleanMessage
+}
+
+task Compile -depends Clean -description "Compile the all solution"{
+	Write-Host $CompileMessage
+}
+
+task Test -depends Compile, Clean -description "Runs the unit test"{
+	Write-Host $TestMessage
+}
