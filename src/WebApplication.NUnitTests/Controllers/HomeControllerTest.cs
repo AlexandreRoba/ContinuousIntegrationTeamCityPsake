196d35b8e7ea134fa108b36711886b509c62309c
Alexandre Roba <alexandre.roba@arobanet.be>
2016-05-24 11:33:03 +0200

    Added Nunit and Xunit project test

---
+++ src/WebApplication.NUnitTests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace WebApplication.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void About_WhenCalled_ShouldFillInTheMessage()
+        {
+            var sut = new HomeController();
+
+            sut.About();
+
+            Assert.IsNotEmpty(sut.ViewBag.Message);
+        }
+    }
+}
---
+++ src/WebApplication.XUnitTests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace WebApplication.Controllers
+{
+
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void About_WhenCalled_ShouldAddMessageToTheViewBag()
+        {
+            var sut = new HomeController();
+
+            sut.About();
+
+            Assert.NotEmpty(sut.ViewBag.Message);
+        }
+    }
+}
